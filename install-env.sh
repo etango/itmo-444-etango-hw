fa824e8a5198f5bbd3a5c912013b7fae4539e11c
etango <etang315@gmail.com>
2015-10-04 20:05:25 -0500

    Create install-env.sh

---
+++ install-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install apache2 -y
+sudo apt-get git -y
+
+sudo echo "Hello!" > /tmp/hello.txt
+sudo git clone https://github.com/etango/itmo-444-etango-hw.git
+
